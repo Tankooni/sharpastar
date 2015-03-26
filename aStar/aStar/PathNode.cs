38cbb86954e85854b6d4e9a44945b1dd56e193fd
Zack Wheeler <913966+Tankooni@users.noreply.github.com>
2015-03-25 22:09:08 -0400

    Hey look. I did it.

---
+++ aStar/aStar/PathNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Priority_Queue;
+
+namespace aStar
+{
+	public class PathNode : PriorityQueueNode
+	{
+		/// <summary>
+		/// Enter a node, receive a list of nodes connected to node and how much effort it is to travel to that node from input node
+		/// </summary>
+		public readonly static Dictionary<PathNode, List<Tuple<PathNode, float>>> ConnectedNodes = new Dictionary<PathNode, List<Tuple<PathNode, float>>>();
+
+		public bool Enabled;
+		public MapCell attachedObject;
+		public float X { get; private set; }
+		public float Y { get; private set; }
+
+		public PathNode(MapCell mapCell, List<Tuple<PathNode, float>> connections, float x = 0, float y = 0, bool enabled = true)
+		{
+			ConnectedNodes.Add(this, connections ?? new List<Tuple<PathNode, float>>());
+			Enabled = enabled;
+			attachedObject = mapCell;
+			X = x;
+			Y = y;
+		}
+
+		//~PathNode()
+		//{
+		//	Console.Write("Destroyed");
+		//	//ConnectedNodes.Remove(this);
+		//}
+
+
+	}
+}
--- aStar/aStar/TileMap.cs
+++ aStar/aStar/TileMap.cs
@@ -7,7 +7,9 @@ using System.Threading.Tasks;
 using Indigo;
 using Indigo.Core;
 using Indigo.Graphics;
-using MoogGeneral.Collections;
+using Priority_Queue;
+using Indigo.Components;
+using Indigo.Inputs;
 
 namespace aStar
 {
@@ -19,17 +21,17 @@ namespace aStar
 	public class TileMap : Entity
 	{
 		public List<MapRow> Rows = new List<MapRow>();
-		public int MapWidth = 50;
-		public int MapHeight = 50;
+		public int MapWidth = 35;
+		public int MapHeight = 35;
 
 		const int TileSize = 16;
 
 		public TileMap()
 		{
-			var startIndex = Tuple.Create<int, int>((int)(FP.Random * 50), (int)(FP.Random * 50));
-			var endIndex = Tuple.Create<int, int>((int)(FP.Random * 50), (int)(FP.Random * 50));
+			var startIndex = Tuple.Create<int, int>(FP.Rand(MapWidth), FP.Rand(MapHeight));
+			var endIndex = Tuple.Create<int, int>(FP.Rand(MapWidth), FP.Rand(MapHeight));
 			while(startIndex.Item1 == endIndex.Item1 && startIndex.Item1 == endIndex.Item2)
-				endIndex = Tuple.Create<int, int>((int)(FP.Random * 50), (int)(FP.Random * 50));
+				endIndex = Tuple.Create<int, int>(FP.Rand(MapWidth), FP.Rand(MapHeight));
 			for (int y = 0; y < MapHeight; y++)
 			{
 				MapRow row = new MapRow();
@@ -41,30 +43,32 @@ namespace aStar
 					else if (x == endIndex.Item1 && y == endIndex.Item2)
 						tileType = TileType.End;
 					else
-						tileType = FP.Choose.EnumWeighted<TileType>(0, 0, 0, 1, .3);
+						tileType = FP.Choose.EnumWeighted<TileType>(0, 0, 0, 1, .65);
 					MapCell mc = new MapCell(tileType, TileSize, Tuple.Create<int, int>(x, y))
 					{
 						X = x * TileSize,
 						Y = y * TileSize
 					};
+					mc.SetPathNode(new PathNode(mc, null, x, y));
 					this.AddComponent<Image>(mc);
 					row.Columns.Add(mc);
 				}
 				Rows.Add(row);
 			}
 
-			foreach (var tile in SelectAstarPath(startIndex, endIndex, false))
-				tile.ChangeTileType(TileType.Path);
-			//foreach (var mc in SelectTilesAroundTile(Tuple.Create<int, int>(0, 0), true))
-			//	mc.Item1.ChangeTileType(TileType.Path);
-			//foreach (var mc in SelectTilesAroundTile(Tuple.Create<int, int>(5, 5), false))
-			//	mc.Item1.ChangeTileType(TileType.Path);
-			//foreach (var mc in SelectTilesAroundTile(Tuple.Create<int, int>(49, 0), true))
-			//	mc.Item1.ChangeTileType(TileType.Path);
-			//foreach (var mc in SelectTilesAroundTile(Tuple.Create<int, int>(0, 49), true))
-			//	mc.Item1.ChangeTileType(TileType.Path);
-			//foreach (var mc in SelectTilesAroundTile(Tuple.Create<int, int>(49, 49), true))
-			//	mc.Item1.ChangeTileType(TileType.Path);
+			foreach (MapRow mr in Rows)
+				foreach (MapCell mc in mr.Columns)
+					PathNode.ConnectedNodes[mc.MyNode] = SelectTilesAroundTile(mc.Index.Item1, mc.Index.Item2);
+
+			CoroutineHost coHost = AddComponent<CoroutineHost>(new CoroutineHost());
+			coHost.Start(Things(startIndex, endIndex));
+
+			//foreach (var bob in SelectTilesAroundTile(startIndex.Item1, startIndex.Item2))
+			//	bob.Item1.attachedObject.ChangeTileType(TileType.Path);
+
+			
+			//foreach (var tile in SelectAstarPath(startIndex, endIndex, false))
+			//	tile.ChangeTileType(TileType.Path);
 		}
 
 		/// <summary>
@@ -73,24 +77,25 @@ namespace aStar
 		/// <param name="curPosition"></param>
 		/// <param name="includeDiag">This will determine if diagnals are to be included. Defaults to false</param>
 		/// <returns></returns>
-		public IEnumerable<Tuple<MapCell, float>> SelectTilesAroundTile(Tuple<int, int> curPosition, bool includeDiag = false)
+		public List<Tuple<PathNode, float>> SelectTilesAroundTile(int centerX, int centerY, bool includeDiag = false)
 		{
+			List<Tuple<PathNode, float>> nodes = new List<Tuple<PathNode, float>>();
 			int[] search = new int[]{-1, 0, 1};
 			foreach (int x in search)
 			{
-				if (curPosition.Item1 + x >= MapWidth)
+				if (centerX + x >= MapWidth)
 					break;
-				if (curPosition.Item1 + x < 0)
+				if (centerX + x < 0)
 					continue;
 				foreach (int y in search)
 				{
-					if (curPosition.Item2 + y >= MapHeight)
+					if (centerY + y >= MapHeight)
 						break;
-					if (curPosition.Item2 + y < 0 || (x == 0 && y == 0))
+					if (centerY + y < 0 || (x == 0 && y == 0))
 						continue;
-					MapCell mapCell = Rows[curPosition.Item1 + x].Columns[curPosition.Item2 + y];
-					//if (MapCell.TileSpeeds[mapCell.TileType] <= 0)
-					//	continue;
+					MapCell mapCell = Rows[centerY + y].Columns[centerX + x];
+					if (MapCell.TileSpeeds[mapCell.TileType] <= 0)
+						continue;
 
 					float moveMultiplier = MapCell.TileSpeeds[mapCell.TileType];
 					if(Math.Abs(x) == 1 && Math.Abs(y) == 1)
@@ -99,12 +104,14 @@ namespace aStar
 							continue;
 						moveMultiplier *= 1.41f;
 					}
-					yield return Tuple.Create<MapCell, float>(mapCell, moveMultiplier);
+					if(mapCell.MyNode.Enabled)
+						nodes.Add(Tuple.Create<PathNode, float>(mapCell.MyNode, moveMultiplier));
 				}
 			}
+			return nodes;
 		}
 
-		public float CalculateHeuristic(int x1, int y1, int x2, int y2)
+		public float CalculateHeuristic(float x1, float y1, float x2, float y2)
 		{
 			return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
 		}
@@ -114,62 +121,67 @@ namespace aStar
 			return Math.Abs(p1.Item1 - p2.Item1) + Math.Abs(p1.Item2 - p2.Item2);
 		}
 
-		public IEnumerable<MapCell> SelectAstarPath(Tuple<int, int> start, Tuple<int, int> end, bool includeDiag)
+		IEnumerator Things(Tuple<int, int> startIndex, Tuple<int, int> endIndex)
 		{
-			MapCell startTile = Rows[start.Item1].Columns[start.Item2];
-			MapCell endTile = Rows[end.Item1].Columns[end.Item2];
-			PriorityQueue<MapCell> frontier = new PriorityQueue<MapCell>();
-			frontier.Push(0, startTile);
-			Dictionary<MapCell, MapCell> cameFrom = new Dictionary<MapCell, MapCell>();
-			Dictionary<MapCell, float> costSoFar = new Dictionary<MapCell, float>();
-
-			cameFrom.Add(startTile, null);
-			costSoFar.Add(startTile, 0);
-
-			if (startTile.Index.Item1 != endTile.Index.Item1 && startTile.Index.Item2 != endTile.Index.Item2)
+			while (!Keyboard.Space.Pressed)
+				yield return null;
+			foreach (var thing in SelectAstarPath(startIndex, endIndex))
 			{
-				while(!frontier.Empty)
+				thing.attachedObject.ChangeTileType(TileType.Path);
+				yield return CoroutineHost.WaitForSeconds(.1f);
+			}
+		}
+
+		public IEnumerable<PathNode> SelectAstarPath(Tuple<int, int> start, Tuple<int, int> end)
+		{
+			PathNode startNode = Rows[start.Item2].Columns[start.Item1].MyNode;
+			PathNode endNode = Rows[end.Item2].Columns[end.Item1].MyNode;
+			HeapPriorityQueue<PathNode> frontier = new HeapPriorityQueue<PathNode>(PathNode.ConnectedNodes.Keys.Count);
+
+			frontier.Enqueue(startNode, 0);
+
+			Dictionary<PathNode, PathNode> cameFrom = new Dictionary<PathNode, PathNode>();
+			Dictionary<PathNode, float> costSoFar = new Dictionary<PathNode, float>();
+
+			cameFrom.Add(startNode, null);
+			costSoFar.Add(startNode, 0);
+
+			if (startNode.X != endNode.X && startNode.Y != endNode.Y)
+			{
+				while (frontier.Count > 0)
 				{
-					MapCell current = frontier.Pop();
-					foreach(var nextTile in SelectTilesAroundTile(current.Index))
+					PathNode current = frontier.Dequeue();
+					foreach (var next in PathNode.ConnectedNodes[current])
 					{
-						float newCost = costSoFar[nextTile.Item1] + nextTile.Item2;
-						if (!costSoFar.ContainsKey(nextTile.Item1))
-							costSoFar.Add(nextTile.Item1, newCost);
-						else if (newCost < costSoFar[nextTile.Item1])
-							costSoFar[nextTile.Item1] = newCost;
+						float newCost = costSoFar[current] + next.Item2;
+						if (!costSoFar.ContainsKey(next.Item1))
+							costSoFar.Add(next.Item1, newCost);
+						else if (newCost < costSoFar[next.Item1])
+							costSoFar[next.Item1] = newCost;
 						else
 							continue;
-						float priority = newCost + CalculateHeuristic(endTile.Index, nextTile.Item1.Index);
-						frontier.Push(priority, nextTile.Item1);
-						if (cameFrom.ContainsKey(nextTile.Item1))
-							cameFrom[nextTile.Item1] = current;
+						float priority = newCost + CalculateHeuristic(endNode.X, endNode.Y, startNode.X, startNode.Y);
+						frontier.Enqueue(next.Item1, priority);
+						
+						if (cameFrom.ContainsKey(next.Item1))
+							cameFrom[next.Item1] = current;
 						else
-							cameFrom.Add(nextTile.Item1, current);
+							cameFrom.Add(next.Item1, current);
 					}
 				}
 			}
 
-			MapCell currentTile = endTile;
-			while (currentTile.Index.Item1 != endTile.Index.Item1 && currentTile.Index.Item2 != endTile.Index.Item2)
-				yield return currentTile = cameFrom[currentTile];
+			PathNode currentNode = endNode;
+			while (startNode.X != currentNode.X || startNode.Y != currentNode.Y)
+			{
+				if (cameFrom.ContainsKey(currentNode))
+				{
+					yield return currentNode = cameFrom[currentNode];
+				}
+				else
+					break;
+			}
 
 		}
-		//public override void Added()
-		//{
-		//	base.Added();
-
-		//	foreach (MapRow mr in Rows)
-		//		foreach (MapCell mc in mr.Columns)
-		//			FP.World.Add(mc);
-		//}
-		//public override void Removed()
-		//{
-		//	base.Removed();
-		//	foreach (MapRow mr in Rows)
-		//		//this.World.AddList(mr.Columns);
-		//		foreach (MapCell mc in mr.Columns)
-		//			FP.World.Remove(mc);
-		//}
 	}
 }
